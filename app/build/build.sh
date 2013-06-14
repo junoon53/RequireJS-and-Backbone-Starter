5c94e659c5b1675cb4e86e87f8333b0ba9a9ac52
Vikram Pawar <junoon.53@gmail.com>
2013-06-14 13:34:32 +0530

    updated build.sh

--- app/build/build.sh
+++ app/build/build.sh
@@ -4,6 +4,9 @@ mv scripts/vendor/requirejs/require.js require.js
 rm -rf scripts/vendor/* build scripts/views scripts/models scripts/collections build.txt
 mkdir scripts/vendor/requirejs && mv require.js scripts/vendor/requirejs/require.js
 mv css/style.css style.css && rm -rf css/* && mv style.css css/style.css
+mkdir -p scripts/vendor/bootstrap/docs/assets
 cp -r ../app/scripts/vendor/bootstrap/docs/assets/img scripts/vendor/bootstrap/docs/assets 
+mkdir -p scripts/vendor/font-awesome/font
+cp -r ../app/scripts/vendor/font-awesome/font scripts/vendor/font-awesome/font
 
 
